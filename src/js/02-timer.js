e9a22a1c51d16c7ac7bd01ef53508d59ceebe64d
Kamil Przebieracz <dmjs23@gmail.com>
2022-04-06 15:05:45 +0200

    add to github

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -88,9 +88,9 @@ console.log(Date());
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
-  const minute = second * 60; // 60 000
-  const hour = minute * 60; // 3 600 000
-  const day = hour * 24; // 86 400 000
+  const minute = second * 60; 
+  const hour = minute * 60; 
+  const day = hour * 24; 
 
   // Remaining days
   const days = Math.floor(ms / day);
